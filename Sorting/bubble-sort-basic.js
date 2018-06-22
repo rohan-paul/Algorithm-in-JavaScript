bfb9cb1a5f481466e0a320bb06a5dc86dcffd8fe
Rohan Paul <rohanpaul2@gmail.com>
2018-06-22 20:09:13 +0530

    adding more optimal solution to bubble sort

--- Sorting/bubble-sort-basic.js
+++ Sorting/bubble-sort-basic.js
@@ -1,5 +1,6 @@
-let arr = Array.from({length: 20}, () => Math.floor(Math.random() * 20))
-console.log(arr);
+let arr = Array.from({ length: 20}, () => Math.floor(Math.random() * 20))
+// console.log(arr);
+var array1 = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 
 // swap helper function
 swap = (arr, i, j) => {
@@ -15,25 +16,47 @@ bubbleSortBasicAscending = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 1; j < arr.length; j++) {
       if (arr[j] < arr[j - 1]) {
-        swap(arr, j, j-1);
+        swap(arr, j, j - 1);
       }
     }
   }
   return arr;
 }
 
-console.log(bubbleSortBasicAscending(arr));
+console.log(bubbleSortBasicAscending(array1));
 
 bubbleSortBasicDescending = (arr) => {
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 1; j < arr.length; j++) {
       if (arr[j] > arr[j - 1]) {
-        swap(arr, j, j-1);
+        swap(arr, j, j - 1);
       }
     }
   }
   return arr;
 }
 
-console.log(bubbleSortBasicDescending(arr));
+console.log(bubbleSortBasicDescending(array1));
+
+/* A more optimal solution, by reducing some of the loop execution are not done in this solution.
+So, here, I only do the loops and swaps for the cases when I find a mis-placed element, i.e. larger-element placed before smaller in an ascending sort
+*/
+bubbleSortAscending = arr => {
+
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] && arr[i + 1] && (arr[i] > arr[i + 1])) {
+        swap(arr, i, i + 1);
+        swapped = true;
+      }
+    }
+  } while (swapped)
+  return arr;
+}
+
+
+console.log(bubbleSortAscending(array1));
